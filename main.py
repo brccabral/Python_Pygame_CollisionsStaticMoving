b9a524fdfd6e0b1eef871ba095933b8776381a99
Bruno Cabral <cabral.brc@gmail.com>
2022-03-29 20:38:15 -0700

    one rect bounces on screen

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import pygame, sys
 
+
+def bouncing_rect():
+    global x_speed, y_speed
+
+    moving_rect.x += x_speed
+    moving_rect.y += y_speed
+
+    # collision with screen borders
+    if moving_rect.right >= screen_width or moving_rect.left <= 0:
+        x_speed *= -1
+    if moving_rect.bottom >= screen_width or moving_rect.top <= 0:
+        y_speed *= -1
+
+    pygame.draw.rect(screen, "white", moving_rect)
+    pygame.draw.rect(screen, "red", other_rect)
+
+
 pygame.init()
 clock = pygame.time.Clock()
 screen_width, screen_height = 800, 800
@@ -18,7 +35,6 @@ while True:
             sys.exit()
 
     screen.fill((30, 30, 30))
-    pygame.draw.rect(screen, "white", moving_rect)
-    pygame.draw.rect(screen, "red", other_rect)
+    bouncing_rect()
     pygame.display.flip()
     clock.tick(60)
