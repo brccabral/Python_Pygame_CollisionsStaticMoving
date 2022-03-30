c1039784220c95c4cc974d82232cd96a3ead3d9f
Bruno Cabral <cabral.brc@gmail.com>
2022-03-29 20:35:16 -0700

    draw rects

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ clock = pygame.time.Clock()
 screen_width, screen_height = 800, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+moving_rect = pygame.Rect(350, 350, 100, 100)
+x_speed, y_speed = 5, 4
+
+other_rect = pygame.Rect(300, 600, 200, 100)
+other_speed = 2
+
 while True:
     for event in pygame.event.get([pygame.QUIT]):
         if event.type == pygame.QUIT:
@@ -12,5 +18,7 @@ while True:
             sys.exit()
 
     screen.fill((30, 30, 30))
+    pygame.draw.rect(screen, "white", moving_rect)
+    pygame.draw.rect(screen, "red", other_rect)
     pygame.display.flip()
     clock.tick(60)
